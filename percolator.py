977658202509f6c298e7b5eae0e13773862b4506
Egan  Lai <eganlai@Egans-MacBook-Pro.local>
2020-11-30 20:29:45 -0800

    intial

--- percolator.py
+++ percolator.py
@@ -1,19 +1,45 @@
 import random
+import benchmark.py
 
 class PercolationPlayer:
+
+	degrees = {vertex: 0 for vertex in graph.V}
+	for edge in edges.V:
+		degrees[edge.a] +=1
+		degrees[edge.b] += 1
+
 	# `graph` is an instance of a Graph, `player` is an integer (0 or 1).
 	# Should return a vertex `v` from graph.V where v.color == -1
 	def ChooseVertexToColor(graph, player):
-    	return random.choice([v for v in graph.V if v.color == -1])
+		degrees1 = {vertex: 0 for vertex in graph.V}
+		for edge in edges.V:
+			degrees1[edge.a] +=1
+			degrees1[edge.b] += 1
+		max_key = max(degrees1, key=degrees1.get)
+		while max_key.color != -1:
+			degrees1.pop(max_key)
+			max_key = max(degrees1, key=degrees1.get)
+		return max_key
+
+			
+
 
 	# `graph` is an instance of a Graph, `player` is an integer (0 or 1).
 	# Should return a vertex `v` from graph.V where v.color == player
 	def ChooseVertexToRemove(graph, player):
-   		pass
+		for v in graph.V:
+			if v.color == player:
+				for e in graph.IncidentEdges(v):
+					if e.a.color != player or e.b.color !=player:
+						return v
+		return random.choice([v for v in graph.V if v.color == player])
+
+
+
 
 # Feel free to put any personal driver code here.
 def main():
-    pass
+	pass
 
 if __name__ == "__main__":
-    main()
+	main()
